5c2020847c08f7ff4dcb6199b387334ce83737d3
KQu1nn <themastertechbr@gmail.com>
2025-08-08 00:09:37 -0300

    Adiciona menu Hamburguer para Mobile

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,24 +1,45 @@
 <template>
-    <div class="fixed top-0 w-full z-50 flex flex-row flex-wrap justify-between items-center px-5 py-5
-         bg-[#09090b]/70 backdrop-blur-md border-b border-[#27272a] text-[#e7e7e7]">
-        <a href="/">
-            <Logo />
-        </a>
-        <ul class="flex flex-row gap-8">
-            <li v-for="(nav, i) in navs" :key="i" class="hover:text-[#6b26d9]">
-                <a :href="nav.url"> {{ nav.name }} </a>
-            </li>
-        </ul>
-        <font-awesome-icon icon="search" class="cursor-pointer text-[#e7e7e7]" />
-    </div>    
+  <div class="fixed top-0 w-full z-50 flex flex-wrap justify-between items-center px-5 py-5
+              bg-[#09090b]/70 backdrop-blur-md border-b border-[#27272a] text-[#e7e7e7]">
+
+    <a href="/">
+      <Logo />
+    </a>
+
+    <ul class="hidden md:flex flex-row gap-8">
+      <li v-for="(nav, i) in navs" :key="i" class="hover:text-[#6b26d9]">
+        <a :href="nav.url"> {{ nav.name }} </a>
+      </li>
+    </ul>
+
+    <font-awesome-icon icon="search" class="cursor-pointer text-[#e7e7e7] md:block hidden" />
+
+    <button @click="menuAberto = !menuAberto" class="md:hidden">
+      <font-awesome-icon :icon="faBars" class="text-2xl" />
+    </button>
+
+    <ul v-if="menuAberto" class="w-full mt-4 flex flex-col gap-4 md:hidden">
+      <li v-for="(nav, i) in navs" :key="'mobile-' + i" class="hover:text-[#6b26d9]">
+        <a :href="nav.url"> {{ nav.name }} </a>
+      </li>
+    </ul>
+  </div>
 </template>
+
+
 <script setup>
-import Logo from '@/components/Logo.vue';
+import { ref } from 'vue'
+import Logo from '@/components/Logo.vue'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+const menuAberto = ref(false)
+
 const navs = [
-    { name: 'Início', url: '/'  },
-    { name: 'IA', url: '#'  },
-    { name: 'Tech', url: '#' },
-    { name: 'Tutoriais', url: '#' },
-    { name: 'Sobre', url: '/about' }
+  { name: 'Início', url: '/' },
+  { name: 'IA', url: '#' },
+  { name: 'Tech', url: '#' },
+  { name: 'Tutoriais', url: '#' },
+  { name: 'Sobre', url: '/about' }
 ]
 </script>
+
--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,8 +1,9 @@
 // src/plugins/fontawesome.js
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faArrowRight, faSearch, faRss, faArrowTrendUp } from '@fortawesome/free-solid-svg-icons'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+import { faArrowRight, faSearch, faRss, faArrowTrendUp, faHeart, faBars } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope as faEnvelopeRegular, faHeart as faHeartRegular, faCalendar, faClock } from '@fortawesome/free-regular-svg-icons'
 import {
   faXTwitter,
@@ -11,7 +12,12 @@ import {
   faFacebook
 } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faXTwitter, faGithub, faInstagram, faFacebook)
-library.add(faRss, faSearch, faArrowRight, faEnvelopeRegular, faCalendar, faClock, faArrowTrendUp, faHeart, faHeartRegular)
+// Adiciona todos os ícones à biblioteca
+library.add(
+  faXTwitter, faGithub, faInstagram, faFacebook,
+  faRss, faSearch, faArrowRight, faEnvelopeRegular,
+  faCalendar, faClock, faBars, faArrowTrendUp,
+  faHeart, faHeartRegular
+)
 
 export { FontAwesomeIcon }
